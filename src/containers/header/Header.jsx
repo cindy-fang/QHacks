1406b70c98a340707db7527441a4c2a9bc242eaa
Cindy Fang <cindy.fang@torontomu.ca>
2024-02-03 16:05:30 -0500

    css changes

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,18 +3,18 @@ import ai from '../../assets/possibility.png';
 import './header.css';
 
 const Header = () => (
-  <div className="gpt3__header section__padding" id="home">
-    <div className="gpt3__header-content">
+  <div className="eyetunes__header section__padding" id="home">
+    <div className="eyetunes__header-content">
       <h1 className="gradient__text">EyeTunes: Redefining Vision-Driven Interaction.</h1>
       <p>EyeTunes is the future of interactivity. With our VR headset program, you can transform any object into a multimedia experience just by looking at it. Say goodbye to traditional interfaces and hello to a world where your gaze is your remote control.</p>
 
-      <div className="gpt3__header-content__input">
+      <div className="eyetunes__header-content__input">
         <input type="email" placeholder="Your Email Address" />
         <button type="button">Get Started</button>
       </div>
     </div>
 
-    <div className="gpt3__header-image">
+    <div className="eyetunes__header-image">
       <img src={ai} />
     </div>
   </div>
