78cfb2385aac1c8f82f4f81b982ee9f0d08fc436
Cindy Fang <cindy.fang@torontomu.ca>
2024-02-03 14:06:09 -0500

    web page initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import { Footer, Info, Header } from './containers';
+import { Navbar } from './components';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-}
+    <Info />
+    <Footer />
+  </div>
+);
 
 export default App;
---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others?</h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>Made with 💖☕🌊🍞🥛by Cindy, Ian, Edison, William</p>
+    </div>
+  </div>
+);
+
+export default Footer;
